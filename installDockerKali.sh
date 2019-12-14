25be8ab8593f578c8a952ac7a3a8df6045421666
BoniW <32690295+BoniWPy@users.noreply.github.com>
2019-12-14 18:50:56 +0700

    Update installDockerKali.sh

--- installDockerKali.sh
+++ installDockerKali.sh
@@ -4,9 +4,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
 apt-get install docker-ce -y
+apt install sudo -y
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 adduser $USER
 usermod -aG docker $USER
 systemctl enable docker
-docker pull kalilinux/kali-linux-docker
-docker run -ti kalilinux/kali-linux-docker /bin/bash
 echo "BYE..GOOO.."
